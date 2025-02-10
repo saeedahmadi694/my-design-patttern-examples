e04fc3a2a263c320059ab901fa9d144bbbc3c2d2
Saeed Ahmadi <saeedahmadi694@gmail.com>
2025-02-10 18:38:09 +0330

    value  object design pattern

---
+++ DesignPattern.sample/Patterns/ValueObject/Money.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.sample.Patterns.ValueObject;
+
+public class Money
+{
+    public decimal Amount { get; private set; }
+    public string Currency { get; private set; }
+    public Money(decimal amount, string currency)
+    {
+        if (amount < 0)
+            throw new ArgumentException("Amount cannot be negative.");
+        if (string.IsNullOrWhiteSpace(currency))
+            throw new ArgumentException("Currency cannot be empty.");
+
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public static bool operator ==(Money money1, Money money2)
+    {
+        return money1.Amount == money2.Amount && money1.Currency == money2.Currency;
+    }
+
+    public static bool operator !=(Money money1, Money money2)
+    {
+        return !(money1 == money2);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+        var money = (Money)obj;
+        return Amount == money.Amount && Currency == money.Currency;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Amount, Currency);
+    }
+}
---
+++ DesignPattern.sample/Patterns/ValueObject/ValueObjectUsage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.sample.Patterns.ValueObject;
+
+public class ValueObjectUsage
+{
+    public void Run()
+    {
+        var money1 = new Money(100, "USD");
+        var money2 = new Money(200, "EUR");
+
+        if (money1 == money2)
+        {
+            Console.WriteLine("The two money objects are equal.");
+        }
+        else
+        {
+            Console.WriteLine("The two money objects are not equal.");
+        }
+    }
+}
