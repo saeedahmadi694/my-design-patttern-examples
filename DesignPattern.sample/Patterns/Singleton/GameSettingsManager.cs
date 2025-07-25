7151f939276c168b1e3554916d88de6b53febb82
Saeed Ahmadi <saeedahmadi694@gmail.com>
2025-07-25 20:46:30 +0330

    feat(singleton): add singleton design pattern

---
+++ DesignPattern.sample/Patterns/Singleton/GameSettingsManager.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.sample.Patterns.Singleton;
+public class GameSettingsManager
+{
+    private static GameSettingsManager _instance;
+
+    private GameSettingsManager()
+    {
+        Difficulty = "Normal";
+        Language = "en";
+        Volume = 0.8f;
+    }
+
+    public static GameSettingsManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new GameSettingsManager();
+
+            return _instance;
+        }
+    }
+
+    public string Difficulty { get; set; }
+    public string Language { get; set; }
+    public float Volume { get; set; }
+
+}
---
+++ DesignPattern.sample/Patterns/Singleton/SingletonUsage.cs
@@ -0,0 +1,12 @@
+﻿using DesignPattern.sample.Patterns.Decorator.Abilities;
+
+namespace DesignPattern.sample.Patterns.Singleton;
+
+public class SingletonUsage
+{
+    public void Run()
+    {
+        GameSettingsManager.Instance.Language = "fa";
+        Console.WriteLine("Volume: " + GameSettingsManager.Instance.Volume);
+    }
+}
